980e7da6b77f52878e838f801b0adedacb62864d
Guram <86122251+MasterGuram@users.noreply.github.com>
2025-02-06 11:23:06 +0400

    commit About

--- js/main.js
+++ js/main.js
@@ -24,7 +24,161 @@
     menu.classList.toggle('menu__list--active');
   })
 
-  /// This is home page slider
+  // Imported
+  //header scroll effect
+const header = document.querySelector(".header")
+window.addEventListener("scroll", function(){
+    window.scrollY > 10 ? header.classList.add("sticky") : header.classList.remove("sticky")
+})
+
+//Menu items navigation
+window.addEventListener("scroll", function(){
+    const section = document.querySelectorAll("section")
+    const scrollY = window.scrollY
+
+    section.forEach(function(current){
+        let sectionHight = current.offsetHeight
+        let sectionTop = current.offsetYop - 50
+        let sectionId = current.getAttribute("id")
+        let navItem = document.querySelector(`.nav-item a[href*="${sectionId}"]`)
+
+        if(navItem){
+
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHight){
+            navItem.classList.add("active")
+            } else {
+            navItem.classList.remove("active")
+            }
+        }
+
+        
+    })
+})
+
+// Scroll to top button 
+const scrollToTop = document.querySelector(".scrollToTop")
+window.addEventListener("scroll", function(){
+    scrollToTop.classList.toggle("active", this.window.scrollY > 500)
+})
+
+scrollToTop.addEventListener("click", function(){
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+})
+
+// Dark theme
+const themeBtn = document.querySelector(".theme-btn")
+// Function to get the current theme
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light"
+// Function to get current sign
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon"
+
+themeBtn.addEventListener("click", function(){
+    document.body.classList.toggle("dark-theme")
+    themeBtn.classList.toggle("sun")
+
+    localStorage.setItem("saved-theme", getCurrentTheme())
+    localStorage.setItem("saved-icon", getCurrentIcon())
+})
+
+const savedTheme = localStorage.getItem("saved-theme")
+const savedIcon = localStorage.getItem("saved-icon")
+
+if(savedTheme){
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme")
+    themeBtn.classList[savedIcon === "sun" ? "add": "remove"]("sun")
+}
+
+// Toggle menu
+const navBtn = document.querySelector(".nav-menu-btn")
+const navBar = document.querySelector(".nav")
+const navMenu = document.querySelector(".nav-menu")
+const navLinks = document.querySelectorAll(".nav-link")
+
+navBtn.addEventListener("click", function(){
+    navBtn.classList.toggle("close")
+    navBar.classList.toggle("active")
+    navMenu.classList.toggle("active")
+})
+
+navLinks.forEach(function(link){
+    link.addEventListener("click", function(){
+        navBtn.classList.remove("close")
+        navBar.classList.remove("active")
+        navMenu.classList.remove("active")        
+    })
+})
+
+//Services section - Modal
+const serviceModal = document.querySelectorAll(".service-modal")
+const learnMoreBtn = document.querySelectorAll(".learn-more-btn")
+const modalCloseBtn = document.querySelectorAll(".modal-close-btn")
+
+const modal = function(modalClick){
+    serviceModal[modalClick].classList.add("active")
+}
+
+learnMoreBtn.forEach((button, i) => {
+    button.addEventListener("click", function(){
+        modal(i)
+    })
+})
+
+modalCloseBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        serviceModal.forEach(modal => {
+            modal.classList.remove("active")
+        })
+    })
+})
+
+//Portfolio section - Modal
+const portfolioModals = document.querySelectorAll(".portfolio-model")
+const imgCard = document.querySelectorAll(".img-card")
+const portfolioCloseBtn = document.querySelectorAll(".portfolio-close-btn")
+
+const portfolioModal = function(modalClick){
+    portfolioModals[modalClick].classList.add("active")
+}
+
+imgCard.forEach((button, i) => {
+    button.addEventListener("click", () => {
+        portfolioModal(i)
+    })
+})
+
+portfolioCloseBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        portfolioModals.forEach(modelView => {
+            modelView.classList.remove("active")
+        })
+    })
+})
+
+  var swiper = new Swiper(".client-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  ScrollReveal({
+    reset: true,
+    distance: "60px",
+    duration: 2500,
+    delay: 100
+  })
+
+  ScrollReveal().reveal('.home-info h1, {delay: 500, origin: "left"}');
+
+  /// This is home page slider - to be deleted
   // const swiper = new Swiper(".swiper", {
   //   effect: "fade",
   //   pagination: {
@@ -36,48 +190,3 @@
   //   },
   // }); 
 
-  // Imported
-    // it will change the color of the tabs under 'Choose your car' on new-cars.html and show the content according to categories
-    const tabItem = document.querySelectorAll('.tabs__btn-item');
-    const tabContent = document.querySelectorAll('.tabs__content-item');
-  
-    tabItem.forEach(function (element) {
-      element.addEventListener('click', open);
-    })
-  
-    function open(evt) {
-      const tabTarget = evt.currentTarget;
-      const button = tabTarget.dataset.button;
-  
-      tabItem.forEach(function(item){
-        item.classList.remove('tabs__btn-item--active');
-      });
-  
-      tabTarget.classList.add('tabs__btn-item--active');
-  
-      tabContent.forEach(function(item){
-        item.classList.remove('tabs__content-item--active');
-      });
-  
-      document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
-    }
-  
-    // menu button. Menu will appear when clicked
-    const menuBtn = document.querySelector('.menu__btn');
-    const menu = document.querySelector('.menu__list');
-  
-    menuBtn.addEventListener('click', () => {
-      menu.classList.toggle('menu__list--active');
-    })
-  
-    // This is home page slider
-    const swiper = new Swiper(".swiper", {
-      effect: "fade",
-      pagination: {
-        el: ".swiper-pagination",
-      },
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-    });
